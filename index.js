d9be8257931ce40ca49dd7e79c7f41d46ffe987f
Alice Belchior <alice.belchior23@gmail.com>
2024-04-02 16:28:24 -0300

    Requisição PSOT e criação das views

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
 const porta = 3000;
-// declaração do modulo cliente
-var clienteRouter = require('./routes/clienteRoute'); 
+var Route = require('./routes/Route');
+
+//handlebars
+const hand = require('express-handlebars');
+app.set('view engines', 'html');
+app.engine('handlebars', hand.engine());
+app.set('view engine', 'handlebars')
 
 // Rota utilizada
-app.use('/', clienteRouter);
+app.use(express.urlencoded({
+    extended: true
+})); // declaração da decodificação do padrao de url que faz com que o express deixe os arquivos de retorno mais organizados
+
+// a codificação deve ser processada antes de se chamar a rota
+app.use('/', Route);
 
 // servidor
 app.listen(porta, () =>{
