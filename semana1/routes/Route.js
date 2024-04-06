995e47344f68a5f1d6f64359f99f7b3fa1bdba16
alicebelchior <alice.belchior23@gmail.com>
2024-04-05 22:17:57 -0300

    Parâmetros de URL

---
+++ semana1/routes/Route.js
@@ -0,0 +1,68 @@
+var express = require("express");
+var router = express.Router();
+
+// Lista de cidades
+var cidades = ["Mimoso", "Cachoeiro", "Serra"];
+
+// rotas
+router.get("/clientes", function (req, res) {
+  res.send("Lista de clientes");
+});
+router.get("/", function (req, res) {
+  //rota principal
+  res.send("Bem vindo ao sistema");
+});
+router.post("/", function (req, res) {
+  //msm rota, metodo diferente
+  res.send("inserir registro");
+});
+
+//rota com parâmetro dinâmico
+router.get("/clientes/:nome/:sobrenome?", function (req, res) {
+  //':' é o parametro passado, '?' dps do parametro o torna não obrigatorio
+  res.send(
+    "Seja bem vindo(a) <h1>" +
+      req.params.nome +
+      " " +
+      req.params.sobrenome +
+      "</h1>"
+  );
+});
+
+// rota GET para consultar os dados da lista de cidades
+router.get("/cidades/:id", (req, res) => {
+  /*
+    se alguem colocar esse endereço e no id = 0,
+    ele entra no código, trata e guarda na let id
+    o json vai responder para a tela a cidade[0]
+    */
+  let id = req.params.id;
+  return res.json([cidades[id]]);
+});
+
+// rota POST para envio de novas cidades
+router.post("/cidades/cadastrar", (req, res) => {
+  let nome = req.body.nome;
+  cidades[cidades.length] = nome; // o codigo vai percorrer a lista cidades e jogar o nome do formulário pra dentro da lista
+  return res.json([cidades[cidades.length - 1]]);
+});
+
+// rota para o formulário
+router.get("/cidades", (req, res) => {
+  res.render("form");
+  //res.send("Seja bem vindo a rota de cidades");
+});
+
+//rota com parâmetro de url
+router.get("/par", function (req, res) {
+  let nome = req.query["nome"];
+  if (nome) {
+    res.send("<h1>" + nome + "</h1>");
+  } else {
+    res.send("Não foi localizado nenhuma valor no navegador");
+  }
+  res.send("Nome = " + req.query["nome"]);
+});
+
+// exportação do modulo
+module.exports = router;
