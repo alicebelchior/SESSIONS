77086e314a14a5e73605b8f6fa6130f40c5a51a8
Alice Belchior <alice.belchior23@gmail.com>
2024-04-11 16:14:30 -0300

    criação do model e banco de dados da API

--- semana2/api.js
+++ semana2/api.js
@@ -1,6 +1,16 @@
 //servidor
 const express = require("express");
 const app = express();
+const database = require('./db/db');
+const Funcionario = require('./model/funcionarioModel')
+
+//sincronismo com o bd
+try{
+    database.sync().then(() =>{});
+}
+catch(erro) {
+    console.log('Houve uma falha ao sincronizar com o banco de dados', erro);
+}
 
 //rota principal
 app.get("/", (req, res) =>{
