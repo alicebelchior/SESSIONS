f4f4f784adc97f7841c33a18c399174433e6cbf6
Alice Belchior <alice.belchior23@gmail.com>
2024-04-12 16:27:37 -0300

    criação do controller, create, read, update e delete da API

--- semana2/api.js
+++ semana2/api.js
@@ -1,8 +1,15 @@
 //servidor
 const express = require("express");
 const app = express();
+app.use(express.urlencoded({
+    extended:true
+}));
+app.use(express.json());
+
+//bibliotecas e módulos
 const database = require('./db/db');
-const Funcionario = require('./model/funcionarioModel')
+const Funcionario = require('./model/funcionarioModel');
+const funcionarioConstroller = require('./controller/funcionarioController');
 
 //sincronismo com o bd
 try{
@@ -18,7 +25,19 @@ app.get("/", (req, res) =>{
     // JSON estruturado
     return res.json({
         message: "Olá, mundo!"
-    })
+    });
 });
+
+//rota de create
+app.post('/Cadastrar', funcionarioConstroller.FuncionarioCreate); //essa rota vai chamar a função para criar o objeto funcionario e vai enviar para o controller
+
+//rota de read
+app.get('/Funcionarios', funcionarioConstroller.FuncionarioListar);
+
+//rota de update
+app.put('/Funcionarios/:id', funcionarioConstroller.FuncionarioUpdate);
+
+//roda de delete
+app.delete('/Funcionarios/:id', funcionarioConstroller.FuncionarioDelete)
 
 app.listen(3000);
---
+++ semana2/controller/funcionarioController.js
@@ -0,0 +1,72 @@
+//importação de modulo
+const Funcionario = require('../model/funcionarioModel');
+
+//importação de informações da classe
+module.exports = class funcionarioConstroller{ //tudo que estiver aqui dentro será usado pela aplicação
+    //Create
+    static async FuncionarioCreate(req, res){
+        let nome = req.body.nome;
+        let endereco = req.body.endereco;
+        let telefone = req.body.telefone;
+        let email = req.body.email;
+        let nascimento = req.body.nascimento;
+        const funcionario = { //essa const vai receber os valores que vem da classe
+            nome: nome,
+            endereco: endereco,
+            telefone: telefone,
+            email: email,
+            nascimento: nascimento
+        }
+        await Funcionario.create(funcionario);
+        //retorno para o usuario/solicitante
+        res.json({
+            message: 'Cadastro realizado com sucesso!'
+        });
+    }
+
+    //Read
+    static async FuncionarioListar(req, res){
+        const funcionario = await Funcionario.findAll({ //executa a listagem 
+            raw: true //listagem de todo mundo 
+        });
+        res.json(funcionario) //retorno em via json
+    }
+
+    //Update
+    static async FuncionarioUpdate(req, res){
+        //recebimento do valor do parametro via browser
+        const matricula = req.params.id;
+        //codigo voltado para as variaveis
+        let nome = req.body.nome;
+        let endereco = req.body.endereco;
+        let telefone = req.body.telefone;
+        let email = req.body.email;
+        let nascimento = req.body.nascimento;
+        const funcionario = { 
+            nome: nome,
+            endereco: endereco,
+            telefone: telefone,
+            email: email,
+            nascimento: nascimento
+        }
+        //comparando e verificando se o funcionario possui de fato a matricula igual ao que foi passado
+        await Funcionario.update(funcionario, {
+            where: {matricula: matricula}
+        });
+        res.json({ //retorno se tiver tudo ok
+            message: 'Cadastro atualizado com sucesso!'
+        })
+    }
+
+    //Delete
+    static async FuncionarioDelete(req, res){
+        //recebimento do valor do parametro via browser
+        const matricula = req.params.id;
+        await Funcionario.destroy({
+            where: {matricula: matricula}
+        });
+        res.json({
+            message: 'Funcionário excluído com sucesso!'
+        });
+    }
+}
